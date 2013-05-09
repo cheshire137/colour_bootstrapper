30b7f4110d43f4ec8eceb7b1420d692fe8ac20ef
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-08 21:13:53 -0400

    Adding Rails app with less.js.

---
+++ README.md
@@ -0,0 +1,5 @@
+1) User picks a ColourLovers.com palette.
+2) Grab colors in palette.
+3) Set LESS variables in Twitter Bootstrap to palette colors.
+4) Compile Bootstrap CSS--use less.js? Show preview to user. Let user change color usage, recompile, preview again.
+5) Provide compiled Bootstrap CSS to user.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColourBootstrapper::Application.config.secret_token = '9822bfc084d100afd0381165d8ce0dd8d58338adb78183cb442a8a093edef321bbdae667af6bd384002fa88a930d9e0a55db58f53d7b7acd854e0595cb60dabc'
